7d5c03fde3be3c8cbf03391a759d8601abf42d5b
doamtranvan2001 <doantranvan2001@gmail.com>
2022-03-29 12:57:54 +0700

    excel to DB

--- src/API/StudentAPI.js
+++ src/API/StudentAPI.js
@@ -1,25 +1,25 @@
 import { axiosClient } from "./Link";
-const ProAPI = {
+const StudentAPI = {
     getAll() {
-        const url = `/products`;
+        const url = `/student`;
         return axiosClient.get(url);
     },
     get(id) {
-        const url = `/products/${id}`;
+        const url = `/student/${id}`;
         return axiosClient.get(url);
     },
     add(product) {
-        const url = `/products`;
+        const url = `/student`;
         return axiosClient.post(url, product);
     },
     remove(id) {
-        const url = `/products/${id}`;
+        const url = `/student/${id}`;
         return axiosClient.delete(url);
     },
     upload(id, data) {
-        const url = `/products/${id}`;
+        const url = `/student/${id}`;
         return axiosClient.put(url, data);
     },
 
 };
-export default ProAPI;
+export default StudentAPI;
