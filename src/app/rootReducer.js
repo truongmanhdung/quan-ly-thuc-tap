6e1bcbee60ee62d7ad504917b67d0cff01298383
Dang Trong Truong <dangtrongtruong190197@gmail.com>
2022-03-30 21:04:32 +0700

    feat:login-google ui

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import authSlice from "../features/slice/authSlice";
-import studentReducer from '../features/todoSlide/studentModel'
 
 const rootReducer = combineReducers({
     auth:authSlice,
-    student: studentReducer
 });
 export default rootReducer;
