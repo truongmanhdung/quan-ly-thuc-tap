378be6fcbc36b28465f87505db45691455c8458a
hantv15 <hantvph10714@fpt.edu.vn>
2022-06-29 20:45:50 +0700

    update up link be dev

--- src/API/Link.js
+++ src/API/Link.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 const token = localStorage.getItem("token");
 export const axiosClient = axios.create({
+  // Localhost
   //baseURL: "http://localhost:8000/api",
-  baseURL: "http://139.180.196.74:8000/api",
+  // DEV
+  baseURL: "http://hbgreen.com.vn/api",
+  // Main
+  // baseURL: "http://139.180.196.74:8000/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${token}`,
