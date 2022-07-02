c3bd38048e1213b0410a53eeea007aa3a0a89b05
vis <>
2022-07-02 21:12:11 +0700

    fix time

--- src/pages/business/ListOfBusiness.jsx
+++ src/pages/business/ListOfBusiness.jsx
@@ -1,12 +1,13 @@
-import { Button, Col, Drawer, Row, Select, Table } from 'antd';
-import { array, bool, object } from 'prop-types';
-import React, { useEffect, useState } from 'react';
-import { connect, useDispatch } from 'react-redux';
-import UpFile from '../../components/ExcelDocument/UpFile';
-import { getBusiness } from '../../features/businessSlice/businessSlice';
-import { getListMajor } from '../../features/majorSlice/majorSlice';
-import { getSemesters } from '../../features/semesters/semestersSlice';
-import styles from './bussiness.module.css';
+import { Button, Col, Drawer, Row, Select, Table } from "antd";
+import { array, bool, object } from "prop-types";
+import React, { useEffect, useState } from "react";
+import { connect, useDispatch } from "react-redux";
+import SemestersAPI from "../../API/SemestersAPI";
+import UpFile from "../../components/ExcelDocument/UpFile";
+import { getBusiness } from "../../features/businessSlice/businessSlice";
+import { getListMajor } from "../../features/majorSlice/majorSlice";
+import { getSemesters } from "../../features/semesters/semestersSlice";
+import styles from "./bussiness.module.css";
 const { Option } = Select;
 const { Column } = Table;
 const ListOfBusiness = ({
@@ -23,67 +24,80 @@ const ListOfBusiness = ({
     page: 1,
     limit: 20,
     campus_id: infoUser.manager.campus_id,
-    smester_id: defaultSemester && defaultSemester._id ? defaultSemester._id : '',
+    smester_id:
+      defaultSemester && defaultSemester._id ? defaultSemester._id : "",
   });
-  const [majorImport, setMajorImport] = React.useState('');
+  const [majorImport, setMajorImport] = React.useState("");
   useEffect(() => {
     dispatch(getSemesters());
     dispatch(getListMajor());
-    dispatch(getBusiness(page));
-  }, [page, dispatch]);
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (page?.smester_id && page?.smester_id.length > 0) {
+      dispatch(
+        getBusiness({
+          ...page,
+        })
+      );
+    } else {
+      SemestersAPI.getDefaultSemester()
+        .then((res) => {
+          if (res.status === 200) {
+            dispatch(
+              getBusiness({
+                ...page,
+                smester_id: res.data._id,
+              })
+            );
+          }
+        })
+        .catch(() => {});
+    }
+  }, [page]);
   const columns = [
     {
-      title: 'Mã',
-      dataIndex: 'code_request',
+      title: "Mã",
+      dataIndex: "code_request",
       width: 50,
-      fixed: 'left',
+      fixed: "left",
     },
     {
-      title: 'Tên doanh nghiệp',
-      dataIndex: 'name',
+      title: "Tên doanh nghiệp",
+      dataIndex: "name",
       width: 150,
-      fixed: 'left',
+      fixed: "left",
     },
     {
-      title: 'Vị trí thực tập',
-      dataIndex: 'internshipPosition',
+      title: "Vị trí thực tập",
+      dataIndex: "internshipPosition",
     },
     {
-      title: 'Số lượng',
-      dataIndex: 'amount',
+      title: "Số lượng",
+      dataIndex: "amount",
     },
 
     {
-      title: 'Địa chỉ thực tập',
-      dataIndex: 'address',
+      title: "Địa chỉ thực tập",
+      dataIndex: "address",
     },
     {
-      title: 'Ngành',
-      dataIndex: 'majors',
+      title: "Ngành",
+      dataIndex: "majors",
       render: (val) => val?.name,
     },
     {
-      title: 'Yêu cầu',
-      dataIndex: 'request',
+      title: "Yêu cầu",
+      dataIndex: "request",
       width: 400,
     },
     {
-      title: 'Chi tiết',
-      dataIndex: 'description',
+      title: "Chi tiết",
+      dataIndex: "description",
       width: 400,
     },
   ];
 
-  // const handleStandardTableChange = (key, value) => {
-  //   const newValue =
-  //     value.length > 0 || (value < 11 && value !== '')
-  //       ? {
-  //           ...filter,
-  //           [key]: value,
-  //         }
-  //       : omit(filter, [key]);
-  //   setFiler(newValue);
-  // };
   const [visible, setVisible] = useState(false);
   const openVisible = () => {
     setVisible(true);
@@ -91,25 +105,58 @@ const ListOfBusiness = ({
   const closeVisible = () => {
     setPage({
       ...page,
-      smester_id: '',
     });
     setVisible(false);
   };
   return (
     <div className={styles.status}>
-      <div style={ isMobile ? {
-
-      } : {display: 'flex'}} >
-              <h2 style={{
-                color: 'black'
-              }} >Doanh nghiệp đăng ký</h2>
-          <Button style={isMobile ? {} : {
-            position: 'absolute',
-            right: 70
-          }}  onClick={openVisible}  type='primary'  >
+      <Row
+        style={
+          isMobile
+            ? {}
+            : { alignItems: "center", justifyContent: "space-between" }
+        }
+      >
+        <Col xs={24} sm={24} md={24} lg={8} span={8}>
+          <h2
+            style={{
+              color: "black",
+            }}
+            className="mb-2"
+          >
+            Doanh nghiệp đăng ký
+          </h2>
+        </Col>
+        <Col xs={8} sm={8} md={8} lg={2} span={2} className="mb-2">
+          <p className="m-0">Học kỳ:</p>
+        </Col>
+        <Col xs={16} sm={16} md={16} lg={6} span={6} className="mb-2">
+          <Select
+            className="filter-status"
+            placeholder="Chọn kỳ"
+            onChange={(val) =>
+              setPage({
+                ...page,
+                smester_id: val,
+              })
+            }
+            style={{ width: "100%" }}
+            defaultValue={defaultSemester ? defaultSemester._id : ""}
+          >
+            {listSemesters &&
+              listSemesters?.map((item, index) => (
+                <Option value={item._id} key={index}>
+                  {item.name}
+                </Option>
+              ))}
+          </Select>
+        </Col>
+        <Col className="mb-2" xs={8} sm={8} md={8} lg={4} span={4}>
+          <Button onClick={openVisible} type="primary">
             Thêm Doanh nghiệp
           </Button>
-          </div>
+        </Col>
+      </Row>
       {!isMobile ? (
         <Table
           pagination={{
@@ -123,7 +170,7 @@ const ListOfBusiness = ({
               });
             },
           }}
-          scroll={{ x: 'calc(900px + 50%)' }}
+          scroll={{ x: "calc(900px + 50%)" }}
           rowKey="_id"
           loading={loading}
           columns={columns}
@@ -147,9 +194,9 @@ const ListOfBusiness = ({
           dataSource={listBusiness?.list}
           expandable={{
             expandedRowRender: (record) => (
-              <div style={{ marginTop: '10px' }}>
+              <div style={{ marginTop: "10px" }}>
                 {window.innerWidth < 1023 && window.innerWidth > 739 ? (
-                  ''
+                  ""
                 ) : (
                   <>
                     <p className="list-detail">Email: {record.email}</p>
@@ -173,7 +220,12 @@ const ListOfBusiness = ({
       )}
 
       <div>
-        <Drawer title="Thêm Sinh" placement="left" onClose={closeVisible} visible={visible}>
+        <Drawer
+          title="Thêm Sinh"
+          placement="left"
+          onClose={closeVisible}
+          visible={visible}
+        >
           <Row>
             <Col span={6}>
               <p>Kỳ:</p>
@@ -188,8 +240,9 @@ const ListOfBusiness = ({
                     smester_id: val,
                   })
                 }
+                defaultValue={defaultSemester ? defaultSemester._id : ""}
                 style={{
-                  width: '100%',
+                  width: "100%",
                 }}
               >
                 {listSemesters &&
@@ -204,17 +257,16 @@ const ListOfBusiness = ({
 
           <Row>
             <Col span={6}>
-              {' '}
-              <p>Ngành:</p>{' '}
+              {" "}
+              <p>Ngành:</p>{" "}
             </Col>
             <Col span={18}>
               <Select
                 className="filter-status"
                 onChange={(val) => setMajorImport(val)}
                 style={{
-                  width: '100%',
-            marginTop: 20
-
+                  width: "100%",
+                  marginTop: 20,
                 }}
                 placeholder="Chọn ngành"
               >
@@ -227,19 +279,21 @@ const ListOfBusiness = ({
               </Select>
             </Col>
           </Row>
-          <div style={{
-            width: '50%',
-            margin: 'auto',
-            marginTop: 20
-          }} >
-          <UpFile
-            keys="business"
-            parentMethods={{
-              ...page,
-              majorImport,
-              closeVisible
+          <div
+            style={{
+              width: "50%",
+              margin: "auto",
+              marginTop: 20,
             }}
-          />
+          >
+            <UpFile
+              keys="business"
+              parentMethods={{
+                ...page,
+                majorImport,
+                closeVisible,
+              }}
+            />
           </div>
         </Drawer>
       </div>
@@ -254,12 +308,14 @@ ListOfBusiness.propTypes = {
   listMajors: array,
 };
 
-export default connect(({ auth: { infoUser }, semester, major, business, global }) => ({
-  infoUser,
-  listSemesters: semester.listSemesters,
-  defaultSemester: semester.defaultSemester,
-  listMajors: major.listMajor,
-  listBusiness: business.listBusiness,
-  loading: business.loading,
-  isMobile: global.isMobile,
-}))(ListOfBusiness);
+export default connect(
+  ({ auth: { infoUser }, semester, major, business, global }) => ({
+    infoUser,
+    listSemesters: semester.listSemesters,
+    defaultSemester: semester.defaultSemester,
+    listMajors: major.listMajor,
+    listBusiness: business.listBusiness,
+    loading: business.loading,
+    isMobile: global.isMobile,
+  })
+)(ListOfBusiness);
